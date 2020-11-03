c7c1d13e7472548b3644a2ecef2a93db792f0e17
Raul Romero <raulromerocano@aggienetwork.com>
2020-11-03 14:43:04 +0000

    Implemented Board#draw method

--- lesson_5_programs/tictactoe_improvements.rb
+++ lesson_5_programs/tictactoe_improvements.rb
@@ -44,6 +44,21 @@ class Board
   def reset
     (1..9).each { |key| @squares[key] = Square.new }
   end
+  
+  def draw
+    puts ""
+    puts "     |     |"
+    puts "  #{get_square_at(1)}  |  #{get_square_at(2)}  |  #{get_square_at(3)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{get_square_at(4)}  |  #{get_square_at(5)}  |  #{get_square_at(6)}  "
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{get_square_at(7)}  |  #{get_square_at(8)}  |  #{get_square_at(9)}  "
+    puts "     |     |"
+  end
 end
 
 class Square
@@ -96,17 +111,7 @@ class TTTGame
   def display_board
     puts "You're an #{human.marker}. Computer is an #{computer.marker}"
     puts ""
-    puts "     |     |"
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}  "
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}  "
-    puts "     |     |"
+    board.draw
     puts ""
   end
   
